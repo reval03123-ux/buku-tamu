74621aa47991006a76527db1453703543e5eb580
Reval Ardiansyah <reval03123@gmail.com>
2026-09-23 15:21:05 +0700

    templating app buku tamu dengan sb admin 2

---
+++ index.php
@@ -0,0 +1,16 @@
+    <?php
+        include 'templates/header.php';
+    ?>
+    <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Dashboard Admin</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+<?php
+    include 'templates/footer.php';
+?>
+/end{code}
