a71088e42579480c845c2d6d961ad5cefe967aa3
Reval Ardiansyah <reval03123@gmail.com>
2026-09-24 17:27:34 +0700

    membuat dan menampilkan halaman user

--- templates/footer.php
+++ templates/footer.php
@@ -54,7 +54,7 @@
             <script src="assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
             <!-- Page level custom scripts -->
-            <script src="js/demo/datatables-demo.js"></script>
+            <script src="assets/js/demo/datatables-demo.js"></script>
             <script>
                 $(document).ready(function() {
                     window.setTimeout(function() {
